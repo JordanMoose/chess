96fa0c2fe816201c943b82f15a04c61a3e8456bd
Jordan Moossazadeh <Moose@calvisitor-10-105-162-35.calvisitor.1918.berkeley.edu>
2018-02-13 13:56:36 -0800

    added color and name attributes to piece class; initialized white and black players

--- chess.py
+++ chess.py
@@ -29,16 +29,17 @@ clock = pygame.time.Clock()
 running = True
 FPS = 60
 playtime = 0.0
-# player 1's total playtime
-tot_time_1 = 0.0
-# player 2's total playtime
-tot_time_2 = 0.0
-# player 1's current turn playtime
-time_1 = 0.0
-# player 2's current turn playtime
-time_2 = 0.0
-# who wins the game (0: game not over, 1: player 1 wins, 2: player 2 wins)
+# white's total playtime
+tot_time_w = 0.0
+# black's total playtime
+tot_time_b = 0.0
+# white's current turn playtime
+time_w = 0.0
+# black's current turn playtime
+time_b = 0.0
+# who wins the game (0: game not over, 1: white wins, 2: black wins)
 winner = 0
+
 
 class Player(object):
 
@@ -47,14 +48,18 @@ class Player(object):
         self.pieces_left = ["king", "queen", "rook", "rook", "bishop", "bishop", "knight", "knight",
                             "pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn"]
 
+
 class Piece(object):
 
-    def __init__(self, x_pos, y_pos, movement):
+    def __init__(self, color, name, x_pos, y_pos, movement):
+        self.color = color
+        self.name = name
         self.x_pos = x_pos
         self.y_pos = y_pos
         self.movement = movement
 
-
+white = Player("white")
+black = Player("black")
 
 ############
 # Gameplay #
